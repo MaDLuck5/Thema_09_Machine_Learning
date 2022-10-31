e21b49d1688faee927aefc170f8c982b62312d08
mats <m.p.slik@st.hanze.nl>
2022-11-01 00:49:14 +0100

    further alterations to wrapper

---
+++ Java-Wrapper/src/main/java/nl/bioinf/JavaWrapper.java
@@ -0,0 +1,7 @@
+package nl.bioinf;
+
+public class JavaWrapper {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- Java-Wrapper/src/main/java/nl/bioinf/MLModelWrapper.java
+++
@@ -1,8 +0,0 @@
-package nl.bioinf;
-
-public class MLModelWrapper {
-    public static void main(String[] args) {
-        System.out.println("Hellp World");
-
-    }
-}
